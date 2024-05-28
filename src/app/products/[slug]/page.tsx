57662a2b258e7bdf9ba8d6f0b2dd557a37a32b36
eunbi-kang <ebzzang1905@naver.com>
2024-05-28 18:39:46 +0900

    fixed untracked files

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -3,6 +3,7 @@ type Props = {
 		slug: string;
 	}
 }
+
 export default function PantsPage({params}: Props) {
-	return <h1> {params.slug} 바지 제품 설명 페이지</h1>
+	return <h1>{params.slug} 바지 제품 설명 페이지</h1>
 }
